d63672524602f766ca4c301fcfb7a64b8b8e9609
maxicordoba <maxi_cordoba32@hotmail.com>
2021-11-11 04:01:07 -0300

    se creo la estructura del proyecto y agrego el logo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.png';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <section>
+      <img src={logo} alt="logo de los simpsons" />
+    </section>
+  );
+}
+
+export default App;
